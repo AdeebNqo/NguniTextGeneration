04a422f74518bd0cb790c5999a36d6956164b407
Zola Mahlaza <adeebnqo@gmail.com>
2021-10-19 21:49:59 +0200

    Quick fixes and updates
    1. Added IntelliJ idea config
    2. Fixed the path in ZuluUtils

--- GrammarEngineNguni/src/za/co/mahlaza/research/grammarengine/nguni/zu/ZuluUtils.java
+++ GrammarEngineNguni/src/za/co/mahlaza/research/grammarengine/nguni/zu/ZuluUtils.java
@@ -2,29 +2,34 @@ package za.co.mahlaza.research.grammarengine.nguni.zu;
 
 import za.co.mahlaza.research.grammarengine.base.interfaces.LangUtils;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 public class ZuluUtils implements LangUtils {
 
-    final private String path = "/home/zola/Documents/phd/work/tasks/OWLSIZ/verbaliser/2010.07.17.ZuluSentencesPOStagged(Ukwabelana).txt";
+    final private String path = "/2010.07.17.ZuluSentencesPOStagged(Ukwabelana).txt";
     private HashMap<String, String> wordValues = new HashMap<>();
 
     public ZuluUtils() {
-        try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            stream.forEach(line -> {
-                String[] words = line.split(" ");
-                for (String word : words) {
-                    String[] tokens = word.split("_");
-                    wordValues.put(tokens[0], tokens[1]);
-                }
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        InputStream iStream = ZuluUtils.class.getResourceAsStream(path);
+        BufferedReader bufReader = new BufferedReader(new InputStreamReader(iStream));
+
+        Stream<String> stream = bufReader.lines();
+        stream.forEach(line -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                String[] tokens = word.split("_");
+                wordValues.put(tokens[0], tokens[1]);
+            }
+        });
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,32 @@ import za.co.mahlaza.research.TemplateErrorDetector;
 import za.co.mahlaza.research.errordetection.models.TemplateValidationReport;
 import za.co.mahlaza.research.grammarengine.base.models.template.Template;
 import za.co.mahlaza.research.grammarengine.nguni.zu.ZuluFeatureParser;
+import za.co.mahlaza.research.grammarengine.nguni.zu.ZuluUtils;
 import za.co.mahlaza.research.templateparsing.TemplateReader;
 import za.co.mahlaza.research.templateparsing.URIS;
 
 public class Main {
 
     public static void main(String[] args) {
-        //testTemplateParser();
-        String templatePath = "/home/zola/Documents/ToCT Ontology and Code/OWLSIZ/Templates/";
+        testParseTemplate();
+        testZuluUtils();
+
+    }
+    public static void testZuluUtils () {
+        String noun = "ukudla";
+        ZuluUtils zuluUtils = new ZuluUtils();
+        System.out.println(noun+" is a noun = "+zuluUtils.isNoun(noun));
+    }
+    public static void testParseTemplate () {
+        String templatePath = "/home/zola/Documents/ToCT Ontology and Code/OWLSIZ/Templates/template1.1.ttl";
         String templateName = "templ1.1";
         String templateURI = "http://people.cs.uct.ac.za/~zmahlaza/templates/owlsiz/";
 
         TemplateReader.Init(new ZuluFeatureParser());
         TemplateReader.setTemplateOntologyNamespace(URIS.ToCT_NS);
         Template template = TemplateReader.parseTemplate(templateName, templateURI, templatePath);
-
     }
+
 
     public static Template getTemplate(String templateName, String templateURI, String templatePath) throws UnsupportedOperationException {
         //TODO: find a way to handle features better. There could be different kinds of features. Perhaps need to register different parsers?
